45e398026dfb30be91421db209639e589e2756b8
solty-28 <soltyna28@gmail.com>
2020-04-15 07:04:35 +0000

    [Update]admin/customers,genres,items

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,13 +1,25 @@
 class Admin::CustomersController < ApplicationController
   def index
+  	@customers = Customer.all
   end
 
   def show
+  	@customer = Customer.find(params[:id])
   end
 
   def edit
+  	@customer = Customer.find(params[:id])
   end
 
   def update
+  	@customer = Customer.find(params[:id])
+  	if @customer.update(customer_params)
+  		redirect_to admin_customer_path(@customer)
+  	else
+  		render action: :edit
   end
+
+  private
+  def customer_params
+  	params.require(:customer).permit(:first_name, :family_name, :first_name_kana, :family_name_kana, :phone_number, :email, :is_cancel, shipping_address_attributes:[:id, :postal_code, :address])
 end

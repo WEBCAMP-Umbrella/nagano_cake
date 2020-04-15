5efeaa187456d609425f8607f9a0b029b00e17f8
vagrant <vagrant@localhost.localdomain>
2020-04-15 09:06:45 +0000

    secondupdate

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,28 +1,19 @@
 class Customer::CustomersController < ApplicationController
-  before_action :authenticate_user!
-  before_action :correct_customer, only: [:destroy]
-
-##マイページの表示
+  ##マイページの表示
   def show
-  	@customer = Customer.find(params[:id])
+    @customer = Customer.find(params[:id])
   end
 
 ##退会確認ページの表示
   def cancel
-  	@customer = Customer.find(params[:id])
+    @customer = Customer.find(params[:id])
   end
 
 ##退会ボタンを押す
   def destroy
   	@customer = Customer.find(params[:id])
-    if @customer.destroy
+    if @customer.update
        redirect_to root_path
     end
-
-   def correct_customer
-   	@cutomer = Customer.find(params[:id])
-    redirect_to(root_url) unless @customer == current_user
-   end
-end
 
 end
--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -1,38 +1,4 @@
 class Customer::OrdersController < ApplicationController
-  before_action :authenticate_user!
-  before_action :correct_customer, only: [:create, :confirm, ,:index, :show]
-
-
-##注文情報入力画面を表示する。
-  def new
-    @customer = Customer.find(params[:id])
-    @order =Order.new
-  end
-
-##入力された注文情報を確認画面へ渡し、表示する。
-  def confirm
-   @customer = Customer.find(params[:id])
-   if @customer.update(customer_params)
-      redirect_to customer_orders_path(@customer)
-   else
-      render template: "cart_items/index"
-  end
-
-##購入確定ボタンで、注文情報を確定する。
-  def create
-    @order =Order.new(order_params)
-    @order.customer_id = current_customer.id
-    if @order.save
-      redirect_to admin_orders_path(@order)
-    else
-      @customer = current_customer
-      @cart_items = Cart_item.all
-      render :new
-  end
-
-##購入後画面表示。
-  def thanks
-  end
 
 ##注文履歴一覧画面を表示する。
   def index
@@ -40,15 +6,39 @@ class Customer::OrdersController < ApplicationController
     @order = Order.find(params[:id])
   end
 
+##注文情報入力画面を表示する。
+  def new
+    @customer = Customer.find(params[:id])
+    @order =Order.new
+  end
+
 ##注文履歴詳細画面を表示する。
   def show
     @order = Order.find(params[:id])
   end
 
+##購入確定ボタンで、注文情報を確定する。
+  def create
+    @order =Order.new(order_params)
+    if @order.save
+      redirect_to thanks_customer_orders_path(@order)
+    else
+      @customer = current_customer
+      @cart_items = Cart_item.all
+      render :new
+    end
+  end
 
-  def correct_customer
-    unless @customer == current_user
-      redirect_to_root_path
+  ##入力された注文情報を確認画面へ渡し、表示する。
+  def confirm
+   @customer = Customer.find(params[:id])
+   if @customer.update(customer_params)
+      redirect_to customer_orders_path(@customer)
+   end
+  end
+
+##購入後画面表示。
+  def thanks
   end
 
   private
@@ -58,4 +48,5 @@ class Customer::OrdersController < ApplicationController
   def customer_params
     　params.require(:order).permit(:payment, :delivery, :delivery_address, :addressee)
   end
+
 end

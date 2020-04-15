5aaee9b88b3b5aa59fcd0ae290500c7f0e1189bb
TKbergamot <berg4m0t@gmail.com>
2020-04-15 16:04:25 +0900

    Add admin order_items & orders controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,10 +1,22 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items.all
   end
 
   def index
+    @orders = Order.all
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to @order
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:order_status)
   end
 end

b99d40a97f40a7586c9454fd6c559532e880ef72
solty-28 <soltyna28@gmail.com>
2020-04-15 08:54:36 +0000

    [Fix]customerモデル,itemコントローラー

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -18,7 +18,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to admin_item_path(item.id)
+      redirect_to admin_item_path(@item)
     else
       render action: :new
   end

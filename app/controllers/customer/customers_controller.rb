9bcb34c3740dd6f7a44e9ecf62a5a7e7e49683dc
TKbergamot <berg4m0t@gmail.com>
2020-04-16 15:21:37 +0900

    [Fix] customerコントローラーのfindメソッドを修正、destroyのtoggleを修正

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,19 +1,18 @@
 class Customer::CustomersController < ApplicationController
   ##マイページの表示
   def show
-    @customer = Customer.find(params[:id])
+    @customer = Customer.find(current_customer.id)
   end
 
 ##退会確認ページの表示
   def cancel
-    @customer = Customer.find(params[:id])
+    @customer = Customer.find(current_customer.id)
   end
 
 ##退会ボタンを押す
   def destroy
-  	@customer = Customer.find(params[:id])
-    @customer.update
-    @customer.toggle(:is_cancel)
+  	@customer = Customer.find(current_customer.id)
+    @customer.toggle!(:is_cancel)
     redirect_to root_path
   end
 

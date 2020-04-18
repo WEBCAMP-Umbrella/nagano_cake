a73d91816b3516819e2643f4092d8c008d71f8fd
TKbergamot <berg4m0t@gmail.com>
2020-04-18 12:40:03 +0900

    [add] gemを1つ追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,9 @@ class Item < ApplicationRecord
   has_many :customers, through: :cart_items
   has_many :orders, through: :order_items
   attachment :image
+
+  enum sale_status:{
+    sold_out:     0, #売り切れ
+    sale:         1, #販売中
+  }
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,17 @@
 class Order < ApplicationRecord
   belongs_to :customer
+
+   enum payment:{
+    bank:            0, #銀行振込
+    credit_card:     1, #クレジットカード
+  }
+
+  enum order_status:{
+    waiting:     0, #入金待ち
+    confirm:     1, #入金確認
+    creating:    2, #制作中
+    prepare:     3, #発送準備中
+    sent:        4, #発送済み
+  }
+
 end

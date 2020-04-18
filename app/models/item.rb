06cc0cea00002dda7dcc4edea6ab40572723a56e
TKbergamot <berg4m0t@gmail.com>
2020-04-18 19:08:52 +0900

    [update] 注文情報詳細画面を修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,10 @@ class Item < ApplicationRecord
   has_many :customers, through: :cart_items
   has_many :orders, through: :order_items
   attachment :image
-<<<<<<< HEAD
 
-  enum sale_status:  [:売り切れ, :販売中]
+  enum sale_status:  [
+    :売り切れ,
+    :販売中
+  ]
 
-=======
-  enum sale_status:  [:売り切れ, :販売中]
->>>>>>> f7369cf47da61c481ce01ba9c488dc60351d52c1
 end
